101ac6dd82b44328c540cfd4c85358031b2674f2
Amulya <amulya.daulyal699@gmail.com>
2026-07-30 15:04:28 +1000

    first commit

---
+++ amul.py
@@ -0,0 +1,5 @@
+name = input("Enter your name")
+age = int(input('Your age'))
+carrer = input("What do you wanna be")
+print(f'Hello {name}, considering you are only {age} its very ambigious to be {carrer}')
+
